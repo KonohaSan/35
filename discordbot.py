1d5124a3f0c75d10632b8333c21e055ccdab97bd
KonohaSan <111172183+KonohaSan@users.noreply.github.com>
2023-08-15 05:37:40 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -10,8 +10,18 @@ client = discord.Client(intents=discord.Intents.all())
 host_ip = '0.0.0.0'  # すべてのインターフェースで待ち受け
 host_port = 46490
 
-def send_data_to_ip(client_socket, ip_address):
-    client_socket.send(ip_address.encode('utf-8'))
+# サーバーソケットを作成
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host_ip, host_port))
+server_socket.listen(50)  # 最大50つの接続を待ち受け
+
+print('サーバーが起動しました。')
+
+while True:
+    # クライアントからの接続を待ち受け
+    client_socket, client_address = server_socket.accept()
+    print('クライアントが接続しました。IP:', client_address[0], 'ポート:', client_address[1])
+
     # クライアントにデータを送信
     data_to_send = """30000000 106E46E8
 10000000 4DF9FFFE
@@ -19,6 +29,9 @@ def send_data_to_ip(client_socket, ip_address):
 00120000 00000003
 D0000000 DEADCAFE"""
     client_socket.send(data_to_send.encode('utf-8'))
+    
+    # 接続を閉じる
+    client_socket.close()
 
 @client.event
 async def on_message(message):
@@ -29,8 +42,7 @@ async def on_message(message):
         
         # Send the data to the client with the extracted IP address
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((ip_address, host_port))
-            send_data_to_ip(client_socket, ip_address)
+            client_socket.send(ip_address.encode('utf-8'))
 
 print('Server built')
 client.run(TOKEN)
