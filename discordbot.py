8525dba9c9b07d281abf07c25af2ab744486d47f
KonohaSan <111172183+KonohaSan@users.noreply.github.com>
2023-08-15 03:11:31 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -2,17 +2,14 @@ import discord
 import socket
 import math
 
-TOKEN = '' # TOKENを貼り付け
+TOKEN = 'MTE0MDMxMDQ5NDIwMTI0OTgxNA.GaygYb.GtiDdzWTx5VzA8xA_5cM5ohtvtUXbvtkLObdgM' # TOKENを貼り付け
 
 client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print('ログインしました')
-    
-@client.event
-async def on_ready():
-    print('接続')
+    print('loggined')
+
 # ホストのIPアドレスとポート番号
 host_ip = '0.0.0.0'  # すべてのインターフェースで待ち受け
 host_port = 46490
@@ -20,34 +17,17 @@ host_port = 46490
 # サーバーソケットを作成
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host_ip, host_port))
-server_socket.listen(5)  # 最大5つの接続を待ち受け
+server_socket.listen(50)  # 最大50つの接続を待ち受け
 
-print('サーバーが起動しました。')
+print('Server built')
 
 while True:
     # クライアントからの接続を待ち受け
     client_socket, client_address = server_socket.accept()
-    print('クライアントが接続しました。IP:', client_address[0], 'ポート:', client_address[1])
+    print('client accessed IP:', client_address[0], 'port:', client_address[1])
 
     # クライアントにデータを送信
-    data_to_send = 'Hello, client!'
-    client_socket.send(data_to_send.encode('utf-8'))
-
-    # 接続を閉じる
-    client_socket.close()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!connect'):
-        # コマンドの引数を取得
-        args = message.content.split()
-        ip = args[1] #ipアドレス
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((ip,7331))
-        self = """09020000 13862EF0
+    data_to_send = """09020000 13862EF0
 00002000 00000000
 30000000 106E46E8
 10000000 50000000
@@ -98,16 +78,10 @@ D0000000 DEADCAFE
 00120898 00000000
 0012089C 00000000
 001208A0 00000000
-D0000000 DEADCAFE""".replace(" ","").replace("\n","") #カフェコードから空白と改行を削除
+D0000000 DEADCAFE"""
+    client_socket.send(data_to_send.encode('utf-8'))
 
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((ip,7331))
-        s.send(bytes.fromhex('03'))
-        s.send(bytes.fromhex('10014CFC00000000'))
-        for x in range(math.floor(len(self)/8)):
-            s.send(bytes.fromhex('03'))
-            s.send(bytes.fromhex('0'+format(0x01133000+x*4,'X')+self[x*8:x*8+8]))
-        s.send(bytes.fromhex('03'))
-        s.send(bytes.fromhex('10014CFC00000001'))
+    # 接続を閉じる
+    client_socket.close()
 
 client.run(TOKEN)
